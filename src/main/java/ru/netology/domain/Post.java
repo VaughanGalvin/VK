62b09d929024de8a84a608dd30bd1747bf74c1b6
Vaughan Galvin <vaughan.galvin@gmail.com>
2021-03-11 22:09:31 +0300

    simple classes

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,20 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String author;
+    private String timestamp;
+    private String date;
+    private String picture;
+    private String title;
+    private String banner; // optional
+    private String teaser; // optional
+    private String content;
+    private String like;
+    private String repost;
+    private String view;
+    private String reply;
+
+    //+ getter's & setter's
+
+}
---
+++ src/main/java/ru/netology/domain/Reply.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Reply {
+    private String id;
+    private String author;
+    private String picture;
+    private String timestamp;
+    private String date;
+    private String content;
+    private String like;
+    private String commentsReply;
+
+    //+getter's & setter's
+
+}
