676f014d4ba9404146e49c8dfb3003adbf6f70a8
Vaughan Galvin <vaughan.galvin@gmail.com>
2021-03-11 23:29:16 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] objects;
+
+    public Post[] search(int id, String author, String timestamp, int date, String title, String postType){
+        // no logic
+        return null;
+    }
+
+    public void delete(int id, String author){
+        // no logic
+    }
+}
